25611038f84b766056f41b24c679baebb3e675d2
bncblnc <bncblnc@gmail.com>
2023-02-07 14:01:01 -0300

    header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
-  return <div>TO DO REACT</div>;
+  return (
+    <div>
+      <Header />
+      <h1>Conteúdo</h1>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
